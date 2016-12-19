692d71cd0e4593e85e045469513024b83188d104
yurchenko.s <yurchenko.s@PC66.corp.etecar.ru>
2016-12-19 14:05:35 +0300

    Первый проект с юнит тестом

---
+++ src/main/java/org/test/app/testproject1/Calculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.test.app.testproject1;
+
+/**
+ *
+ * @author yurchenko.s
+ */
+public class Calculator {
+    public int evaluate(String expression) {
+    int sum = 0;
+    for (String summand: expression.split("\\+"))
+      sum += Integer.valueOf(summand);
+    return sum;
+  }
+}
---
+++ src/test/java/org/test/app/testproject1/CalculatorTest.java
@@ -0,0 +1,23 @@
+package org.test.app.testproject1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+/**
+ *
+ * @author yurchenko.s
+ */
+public class CalculatorTest {
+  @Test
+  public void evaluatesExpression() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate("1+2+3");
+    assertEquals(6, sum);
+  }
+}
+
